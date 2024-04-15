94766d5664fbf325a713658d6662de3cbcfd4e87
Serkhani <dak.boniface@gmail.com>
2024-04-15 16:25:11 +0000

    feat(blogcomments): added CommentController and CommentManager classes for managing comments

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,75 @@
+﻿namespace EFCore_BloggingApp;
+using EFCore_BloggingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommentController : ControllerBase
+{
+    private readonly BlogdbContext _context;
+    CommentManager commentManager;
+
+    public CommentController(BlogdbContext context)
+    {
+        _context = context;
+        commentManager = new CommentManager(_context);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetComments()
+    {
+        var tasks = await commentManager.GetAllComments();
+        return Ok(tasks);
+    }
+
+    [HttpGet("{CommentId:int}")]
+    public async Task<IActionResult> GetComment(int CommentId)
+    {
+        try
+        {
+            var task = await commentManager.GetComment(CommentId);
+            return Ok(task);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateComment(Comment comment)
+    {
+        var commentId = await commentManager.CreateComment(comment);
+        return CreatedAtAction(nameof(GetComment), new { CommentId = commentId }, comment);
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> UpdateComment(int CommentId, Comment updatedComment)
+    {
+        try
+        {
+            await commentManager.UpdateComment(CommentId, updatedComment);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteComment(int CommentId)
+    {
+        try
+        {
+            await commentManager.DeleteComment(CommentId);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+
+}
---
+++ Managers/CommentManager.cs
@@ -0,0 +1,56 @@
+﻿using EFCore_BloggingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EFCore_BloggingApp;
+
+public class CommentManager
+{
+    private readonly BlogdbContext _context;
+
+    public CommentManager(BlogdbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Comment>> GetAllComments()
+    {
+        var comments = await _context.Comments.ToListAsync();
+        return comments;
+    }
+    public async Task<Comment> GetComment(int CommentId)
+    {
+        var comment = await _context.Comments.FindAsync(CommentId);
+        if (comment == null)
+        {
+            throw new Exception("The comment does not exist");
+        }
+        return comment;
+    }
+    public async Task<int> CreateComment(Comment comment)
+    {
+        _context.Comments.Add(comment);
+        await _context.SaveChangesAsync();
+        return comment.Commentid;
+    }
+    public async Task UpdateComment(int CommentId, Comment updatedComment)
+    {
+        if (CommentId != updatedComment.Commentid)
+            throw new Exception("The ids do not match");
+        var comment = await _context.Comments.FindAsync(CommentId);
+
+        if (comment == null)
+            throw new Exception("The comment does not exist");
+        comment.Text = updatedComment.Text;
+        _context.Entry(comment).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+    public async Task DeleteComment(int CommentId)
+    {
+        var comment = await _context.Comments.FindAsync(CommentId);
+        if (comment == null)
+            throw new Exception("The comment does not exist");
+        _context.Comments.Remove(comment);
+        await _context.SaveChangesAsync();
+    }
+
+}
